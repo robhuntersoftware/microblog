f986f4ed6ee9be56fec0b7e076a63395c6070bbe
rob <rob@rideco.com>
2018-03-17 14:47:38 -0400

    Finished Tutorial 9
    Can make posts!
    Added the explore page.
    Pagination added.
    Show real posts on profile pages

--- config.py
+++ config.py
@@ -14,3 +14,6 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['admin@example.com']
+
+    # Display settings
+    POSTS_PER_PAGE = 10
